6ffe4986ab4d114f9fdff63605227203502fc31b
MehwishBashir244 <mehwishbashir8011@gmail.com>
2026-08-15 13:27:00 +0500

    Add new lines

--- Python-basics/P1.py
+++ Python-basics/P1.py
@@ -7,3 +7,37 @@ print(name)
 print(age)
 print(type(name))
 print(type(age))
+
+#arithematic
+a = 22
+b = 11
+sum = a+b
+print(sum)
+print(a*b)
+print(a/b)
+print(a**b)
+#relational
+print(a==b)
+print(a!=b)
+#assignment
+num = 22
+num += 2
+print(num)
+#logical
+a = 22
+b = 11
+print(not(a >= b))
+
+val1 = True 
+val2 = False
+print(val1 and val2)
+
+#input
+input("enter your name:")
+
+name = input("enter your name:")
+print('you entered', name)
+
+name = str(input("enter your name:"))
+print('you entered', name)
+print(type(name))
